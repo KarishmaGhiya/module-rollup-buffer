844198fe071108dff9ecd1c4786092063ce74cae
Karishma Ghiya <kaghiya@microsoft.com>
2019-06-12 15:24:27 -0700

    added functionality and test cases

--- lib-aaa/index.ts
+++ lib-aaa/index.ts
@@ -1,7 +1,18 @@
 /// <reference lib="es2015" />
 /// <reference lib="esnext.asynciterable" />
-const buff = require('buffer/').Buffer 
-const bufferConversionResult = buff.from('hello world', 'ascii');
-console.log("lib-aaa printing hex for buffer - 'hello world', 'ascii'");
-console.log(bufferConversionResult.toString('hex'));
-// should print: 68656c6c6f20776f726c64
+import {getBase64} from "lib-bee/index";
+
+const buff = require('buffer/').Buffer;
+
+export function convertToBase64(str: string):string{
+    return getBase64(str);
+}
+
+export function convertToHex(str: string):string{
+	const bufferConversionResult = buff.from(str, 'ascii');
+	console.log("lib-aaa printing hex for buffer - '" + str + "', 'ascii'");
+	const res: string = bufferConversionResult.toString('hex');
+	console.log(res);
+	return res;
+    // should print for 'hello world': 68656c6c6f20776f726c64
+}
